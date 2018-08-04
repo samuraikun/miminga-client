c061dd48c12893c8fa337cd1cb78b86422ed9a11
samuraikun <tazitaziawawa@gmail.com>
2018-08-05 00:30:41 +0900

    add Material UI Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+}
 
 class Header extends Component {
+  constructor() {
+    super();
+  }
+  
   render() {
+    const { classes } = this.props;
     return (
-      <header>
-        <h2>Miminga</h2>
-      </header>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <ToolBar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              Miminga
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </ToolBar>
+        </AppBar>
+      </div>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Header);
